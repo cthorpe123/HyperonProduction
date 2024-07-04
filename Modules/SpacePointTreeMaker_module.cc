e67de4dab5a8f3afe17cc2b4b2ce96c11c923af2
cthorpe123 <c.thorpe@lancaster.ac.uk>
2024-07-04 04:19:45 -0500

    Adding separate track/shower trees to SpacePointTreeMaker

--- Modules/SpacePointTreeMaker_module.cc
+++ Modules/SpacePointTreeMaker_module.cc
@@ -29,6 +29,7 @@
 #include "lardataobj/AnalysisBase/BackTrackerMatchingData.h"
 #include "lardataobj/RecoBase/PFParticle.h"
 #include "lardataobj/RecoBase/Track.h"
+#include "lardataobj/RecoBase/Shower.h"
 #include "lardataobj/RecoBase/Wire.h"
 #include "lardataobj/RecoBase/SpacePoint.h"
 #include "lardataobj/RecoBase/Hit.h"
@@ -85,6 +86,8 @@ class hyperon::SpacePointTreeMaker : public art::EDAnalyzer {
   private:
 
     TTree *t_SpacePointTree;
+    TTree *t_SpacePointTree_Tracks;
+    TTree *t_SpacePointTree_Showers;
 
     // General event info
     unsigned int t_EventID;
@@ -96,6 +99,27 @@ class hyperon::SpacePointTreeMaker : public art::EDAnalyzer {
     std::vector<std::vector<double>> t_PFPSpacePoint_Z;
     std::vector<std::vector<int>> t_PFPSpacePoint_PDG;
     std::vector<std::vector<double>> t_PFPSpacePoint_PDGPur;
+    std::vector<std::vector<std::vector<double>>> t_PFPHit_Channel;
+    std::vector<std::vector<std::vector<double>>> t_PFPHit_Tick;
+    std::vector<std::vector<std::vector<double>>> t_PFPHit_Width;
+
+    std::vector<std::vector<double>> t_PFPSpacePoint_X_Tracks;
+    std::vector<std::vector<double>> t_PFPSpacePoint_Y_Tracks;
+    std::vector<std::vector<double>> t_PFPSpacePoint_Z_Tracks;
+    std::vector<std::vector<int>> t_PFPSpacePoint_PDG_Tracks;
+    std::vector<std::vector<double>> t_PFPSpacePoint_PDGPur_Tracks;
+    std::vector<std::vector<std::vector<double>>> t_PFPHit_Channel_Tracks;
+    std::vector<std::vector<std::vector<double>>> t_PFPHit_Tick_Tracks;
+    std::vector<std::vector<std::vector<double>>> t_PFPHit_Width_Tracks;
+    
+    std::vector<std::vector<double>> t_PFPSpacePoint_X_Showers;
+    std::vector<std::vector<double>> t_PFPSpacePoint_Y_Showers;
+    std::vector<std::vector<double>> t_PFPSpacePoint_Z_Showers;
+    std::vector<std::vector<int>> t_PFPSpacePoint_PDG_Showers;
+    std::vector<std::vector<double>> t_PFPSpacePoint_PDGPur_Showers;
+    std::vector<std::vector<std::vector<double>>> t_PFPHit_Channel_Showers;
+    std::vector<std::vector<std::vector<double>>> t_PFPHit_Tick_Showers;
+    std::vector<std::vector<std::vector<double>>> t_PFPHit_Width_Showers;
 
     //////////////////////////
     //   FHICL PARAMETERS   //
@@ -107,6 +131,8 @@ class hyperon::SpacePointTreeMaker : public art::EDAnalyzer {
     const std::string fPFParticleModuleLabel;
     const std::string fSpacePointModuleLabel;
     const std::string fHitModuleLabel;
+    const std::string fPFParticleTrackAssnLabel;
+    const std::string fPFParticleShowerAssnLabel;
     const std::string fPFParticleSpacePointAssnLabel;
     const std::string fSpacePointHitAssnLabel;
     const std::string fHitTruthAssnLabel;
@@ -122,7 +148,8 @@ hyperon::SpacePointTreeMaker::SpacePointTreeMaker(fhicl::ParameterSet const& p)
   fPFParticleModuleLabel(p.get<std::string>("PFParticleModuleLabel")),
   fSpacePointModuleLabel(p.get<std::string>("SpacePointModuleLabel")),
   fHitModuleLabel(p.get<std::string>("HitModuleLabel")),
-  //fTrackModuleLabel(p.get<std::string>("TrackModuleLabel"),
+  fPFParticleTrackAssnLabel(p.get<std::string>("PFParticleTrackAssnLabel")),
+  fPFParticleShowerAssnLabel(p.get<std::string>("PFParticleShowerAssnLabel")),
   fPFParticleSpacePointAssnLabel(p.get<std::string>("PFParticleSpacePointAssnLabel")),
   fSpacePointHitAssnLabel(p.get<std::string>("SpacePointHitAssnLabel")),
   fHitTruthAssnLabel(p.get<std::string>("HitTruthAssnLabel"))
@@ -148,6 +175,27 @@ void hyperon::SpacePointTreeMaker::analyze(art::Event const& e)
   t_PFPSpacePoint_Z.clear();
   t_PFPSpacePoint_PDG.clear();
   t_PFPSpacePoint_PDGPur.clear();
+  t_PFPHit_Channel.clear();
+  t_PFPHit_Tick.clear();
+  t_PFPHit_Width.clear();
+
+  t_PFPSpacePoint_X_Tracks.clear();
+  t_PFPSpacePoint_Y_Tracks.clear();
+  t_PFPSpacePoint_Z_Tracks.clear();
+  t_PFPSpacePoint_PDG_Tracks.clear();
+  t_PFPSpacePoint_PDGPur_Tracks.clear();
+  t_PFPHit_Channel_Tracks.clear();
+  t_PFPHit_Tick_Tracks.clear();
+  t_PFPHit_Width_Tracks.clear();
+
+  t_PFPSpacePoint_X_Showers.clear();
+  t_PFPSpacePoint_Y_Showers.clear();
+  t_PFPSpacePoint_Z_Showers.clear();
+  t_PFPSpacePoint_PDG_Showers.clear();
+  t_PFPSpacePoint_PDGPur_Showers.clear();
+  t_PFPHit_Channel_Showers.clear();
+  t_PFPHit_Tick_Showers.clear();
+  t_PFPHit_Width_Showers.clear();
 
   // Setup handles/vectors 
   art::Handle<std::vector<recob::PFParticle>> Handle_PFParticle;
@@ -170,6 +218,8 @@ void hyperon::SpacePointTreeMaker::analyze(art::Event const& e)
 
   art::FindManyP<recob::SpacePoint> Assoc_PFParticleSpacePoint(Vect_PFParticle,e,fPFParticleSpacePointAssnLabel);
   art::FindManyP<recob::Hit> Assoc_SpacePointHit(Vect_SpacePoint,e,fSpacePointHitAssnLabel);
+  art::FindManyP<recob::Track> Assoc_PFParticleTrack(Vect_PFParticle,e,fPFParticleTrackAssnLabel);
+  art::FindManyP<recob::Shower> Assoc_PFParticleShower(Vect_PFParticle,e,fPFParticleShowerAssnLabel);
   art::FindMany<simb::MCParticle,anab::BackTrackerHitMatchingData> ParticlesPerHit(Handle_Hit,e,fHitTruthAssnLabel);
 
   // Find the neutrino PFP
@@ -190,15 +240,30 @@ void hyperon::SpacePointTreeMaker::analyze(art::Event const& e)
     if(pfp->Parent() != neutrinoID) continue;
     ctr++;
     if(ctr == 1) continue; // cude way to ignore muon candidate 
+    bool is_track=false,is_shower=false;
+      
+    std::vector<art::Ptr<recob::Track>> pfpTracks = Assoc_PFParticleTrack.at(pfp.key());
+    std::vector<art::Ptr<recob::Shower>> pfpShowers = Assoc_PFParticleShower.at(pfp.key());
+
+    //if(pfpTracks.size() == 1) is_track = true;
+    //if(pfpShowers.size() == 1) is_shower = true;
+     
+    if(pfp->PdgCode() == 13) is_track = true; 
+    else if(pfp->PdgCode() == 11) is_shower = true;
+
+    if(!is_track && !is_shower) 
+      throw cet::exception("SpacePointTreeMaker") << "PFParticle has reconstructed PDG code " << pfp->PdgCode() << std::endl;
 
     std::vector<art::Ptr<recob::SpacePoint>> pfpSpacePoints = Assoc_PFParticleSpacePoint.at(pfp.key());
-    //std::cout << "Found " << pfpSpacePoints.size() << " for this PFP" << std::endl;  
 
     t_PFPSpacePoint_X.push_back(std::vector<double>());
     t_PFPSpacePoint_Y.push_back(std::vector<double>());
     t_PFPSpacePoint_Z.push_back(std::vector<double>());
     t_PFPSpacePoint_PDG.push_back(std::vector<int>());
     t_PFPSpacePoint_PDGPur.push_back(std::vector<double>());
+    t_PFPHit_Channel.push_back(std::vector<std::vector<double>>(3,std::vector<double>()));
+    t_PFPHit_Tick.push_back(std::vector<std::vector<double>>(3,std::vector<double>()));
+    t_PFPHit_Width.push_back(std::vector<std::vector<double>>(3,std::vector<double>()));
 
     // If the spacepoint has an associated hit, truth match it and save
     for(const art::Ptr<recob::SpacePoint> &sp : pfpSpacePoints){
@@ -222,18 +287,47 @@ void hyperon::SpacePointTreeMaker::analyze(art::Event const& e)
       }
       //std::cout << pdg << std::endl;
 
+      if(hits.at(0)->View() != 0 && hits.at(0)->View() != 1 && hits.at(0)->View() != 2)
+        throw cet::exception("SpacePointTreeMaker") << "Hit has unrecognised View number " << hits.at(0)->View() << std::endl;
+        
       t_PFPSpacePoint_X.back().push_back(sp->XYZ()[0]); 
       t_PFPSpacePoint_Y.back().push_back(sp->XYZ()[1]); 
       t_PFPSpacePoint_Z.back().push_back(sp->XYZ()[2]); 
+      t_PFPHit_Channel.back().at(hits.at(0)->View()).push_back(hits.at(0)->Channel());
+      t_PFPHit_Tick.back().at(hits.at(0)->View()).push_back(hits.at(0)->PeakTime());
+      t_PFPHit_Width.back().at(hits.at(0)->View()).push_back(hits.at(0)->EndTick() - hits.at(0)->StartTick());
       t_PFPSpacePoint_PDG.back().push_back(pdg); 
       t_PFPSpacePoint_PDGPur.back().push_back(maxe/e); 
 
     }
 
-  }
+    if(is_track){
+      t_PFPSpacePoint_X_Tracks.push_back(t_PFPSpacePoint_X.back());
+      t_PFPSpacePoint_Y_Tracks.push_back(t_PFPSpacePoint_Y.back());
+      t_PFPSpacePoint_Z_Tracks.push_back(t_PFPSpacePoint_Z.back());
+      t_PFPSpacePoint_PDG_Tracks.push_back(t_PFPSpacePoint_PDG.back());
+      t_PFPSpacePoint_PDGPur_Tracks.push_back(t_PFPSpacePoint_PDGPur.back());
+      t_PFPHit_Channel_Tracks.push_back(t_PFPHit_Channel.back());
+      t_PFPHit_Tick_Tracks.push_back(t_PFPHit_Tick.back());
+      t_PFPHit_Width_Tracks.push_back(t_PFPHit_Width.back());
+    }
+
+    if(is_shower){
+      t_PFPSpacePoint_X_Showers.push_back(t_PFPSpacePoint_X.back());
+      t_PFPSpacePoint_Y_Showers.push_back(t_PFPSpacePoint_Y.back());
+      t_PFPSpacePoint_Z_Showers.push_back(t_PFPSpacePoint_Z.back());
+      t_PFPSpacePoint_PDG_Showers.push_back(t_PFPSpacePoint_PDG.back());
+      t_PFPSpacePoint_PDGPur_Showers.push_back(t_PFPSpacePoint_PDGPur.back());
+      t_PFPHit_Channel_Showers.push_back(t_PFPHit_Channel.back());
+      t_PFPHit_Tick_Showers.push_back(t_PFPHit_Tick.back());
+      t_PFPHit_Width_Showers.push_back(t_PFPHit_Width.back());
+    }
+   
+  } // pfp
 
   // Fill tree! 
   t_SpacePointTree->Fill();
+
 }
 
 //////////////////////////////////////////////////////////////////
@@ -245,17 +339,47 @@ void hyperon::SpacePointTreeMaker::beginJob(){
   art::ServiceHandle<art::TFileService> tfs;
 
   t_SpacePointTree=tfs->make<TTree>("SpacePointTree","Wire Tree");
+  t_SpacePointTree_Tracks=tfs->make<TTree>("SpacePointTree_Tracks","Wire Tree");
+  t_SpacePointTree_Showers=tfs->make<TTree>("SpacePointTree_Showers","Wire Tree");
 
   t_SpacePointTree->Branch("EventID",&t_EventID);
   t_SpacePointTree->Branch("run",&t_run);
   t_SpacePointTree->Branch("subrun",&t_subrun);
   t_SpacePointTree->Branch("event",&t_event);
-
   t_SpacePointTree->Branch("PFPSpacePoint_X",&t_PFPSpacePoint_X);
   t_SpacePointTree->Branch("PFPSpacePoint_Y",&t_PFPSpacePoint_Y);
   t_SpacePointTree->Branch("PFPSpacePoint_Z",&t_PFPSpacePoint_Z);
+  t_SpacePointTree->Branch("PFPHit_Channel","vector<vector<vector<double>>>",&t_PFPHit_Channel);
+  t_SpacePointTree->Branch("PFPHit_Tick","vector<vector<vector<double>>>",&t_PFPHit_Tick);
+  t_SpacePointTree->Branch("PFPHit_Width","vector<vector<vector<double>>>",&t_PFPHit_Width);
   t_SpacePointTree->Branch("PFPSpacePoint_PDG",&t_PFPSpacePoint_PDG);
   t_SpacePointTree->Branch("PFPSpacePoint_PDGPur",&t_PFPSpacePoint_PDGPur);
+
+  t_SpacePointTree_Tracks->Branch("EventID",&t_EventID);
+  t_SpacePointTree_Tracks->Branch("run",&t_run);
+  t_SpacePointTree_Tracks->Branch("subrun",&t_subrun);
+  t_SpacePointTree_Tracks->Branch("event",&t_event);
+  t_SpacePointTree_Tracks->Branch("PFPSpacePoint_X",&t_PFPSpacePoint_X_Tracks);
+  t_SpacePointTree_Tracks->Branch("PFPSpacePoint_Y",&t_PFPSpacePoint_Y_Tracks);
+  t_SpacePointTree_Tracks->Branch("PFPSpacePoint_Z",&t_PFPSpacePoint_Z_Tracks);
+  t_SpacePointTree_Tracks->Branch("PFPHit_Channel","vector<vector<vector<double>>>",&t_PFPHit_Channel_Tracks);
+  t_SpacePointTree_Tracks->Branch("PFPHit_Tick","vector<vector<vector<double>>>",&t_PFPHit_Tick_Tracks);
+  t_SpacePointTree_Tracks->Branch("PFPHit_Width","vector<vector<vector<double>>>",&t_PFPHit_Width_Tracks);
+  t_SpacePointTree_Tracks->Branch("PFPSpacePoint_PDG",&t_PFPSpacePoint_PDG_Tracks);
+  t_SpacePointTree_Tracks->Branch("PFPSpacePoint_PDGPur",&t_PFPSpacePoint_PDGPur_Tracks);
+
+  t_SpacePointTree_Showers->Branch("EventID",&t_EventID);
+  t_SpacePointTree_Showers->Branch("run",&t_run);
+  t_SpacePointTree_Showers->Branch("subrun",&t_subrun);
+  t_SpacePointTree_Showers->Branch("event",&t_event);
+  t_SpacePointTree_Showers->Branch("PFPSpacePoint_X",&t_PFPSpacePoint_X_Showers);
+  t_SpacePointTree_Showers->Branch("PFPSpacePoint_Y",&t_PFPSpacePoint_Y_Showers);
+  t_SpacePointTree_Showers->Branch("PFPSpacePoint_Z",&t_PFPSpacePoint_Z_Showers);
+  t_SpacePointTree_Showers->Branch("PFPHit_Channel","vector<vector<vector<double>>>",&t_PFPHit_Channel_Showers);
+  t_SpacePointTree_Showers->Branch("PFPHit_Tick","vector<vector<vector<double>>>",&t_PFPHit_Tick_Showers);
+  t_SpacePointTree_Showers->Branch("PFPHit_Width","vector<vector<vector<double>>>",&t_PFPHit_Width_Showers);
+  t_SpacePointTree_Showers->Branch("PFPSpacePoint_PDG",&t_PFPSpacePoint_PDG_Showers);
+  t_SpacePointTree_Showers->Branch("PFPSpacePoint_PDGPur",&t_PFPSpacePoint_PDGPur_Showers);
 
   if(fDebug) std::cout << "Finished begin job" << std::endl;
 
